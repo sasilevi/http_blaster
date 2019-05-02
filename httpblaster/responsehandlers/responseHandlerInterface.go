697f02d2005b731e122d7ee0c21ebb3ded427f2a
sasi.levi <sasi.levi@appsflyer.com>
2019-05-02 18:14:38 +0300

    comments

--- httpblaster/responsehandlers/responseHandlerInterface.go
+++ httpblaster/responsehandlers/responseHandlerInterface.go
@@ -7,8 +7,12 @@ import (
 	"github.com/v3io/http_blaster/httpblaster/request_generators"
 )
 
+//IResponseHandler : response handler interface
 type IResponseHandler interface {
+	//Impliment this method in your handler
 	HandlerResponses(global config.Global, workload config.Workload, respoCh chan *request_generators.Response)
+	//This methos is in base object, do not impliment
 	Run(global config.Global, workload config.Workload, respoCh chan *request_generators.Response, wg *sync.WaitGroup, rh IResponseHandler)
+	//Impliment this method to report the log
 	Report() string
 }
